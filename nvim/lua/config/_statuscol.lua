82da29098c58946a0fffe44e559c5c078ab43211
Igor Zeljkovic <zeljkovic18@gmail.com>
2023-07-22 02:53:31 +0200

    Set up statuscolumn

--- nvim/lua/config/_statuscol.lua
+++ nvim/lua/config/_statuscol.lua
@@ -2,14 +2,16 @@ local statuscol = require("statuscol")
 local builtin = require("statuscol.builtin")
 
 statuscol.setup({
-    DiagnosticSignError = builtin.diagnostic_click,
-    DiagnosticSignHint = builtin.diagnostic_click,
-    DiagnosticSignInfo = builtin.diagnostic_click,
-    DiagnosticSignWarn = builtin.diagnostic_click,
-    GitSignsTopdelete = builtin.gitsigns_click,
-    GitSignsUntracked = builtin.gitsigns_click,
-    GitSignsAdd = builtin.gitsigns_click,
-    GitSignsChange = builtin.gitsigns_click,
-    GitSignsChangedelete = builtin.gitsigns_click,
-    GitSignsDelete = builtin.gitsigns_click,
+    segments = {
+        {
+            text = { " %s " },
+            click = "v:lua.ScSa",
+        },
+        {
+            text = { builtin.lnumfunc, " " },
+            condition = { true, builtin.not_empty },
+            click = "v:lua.ScLa",
+        },
+    },
+    ft_ignore = { "startup" },
 })
