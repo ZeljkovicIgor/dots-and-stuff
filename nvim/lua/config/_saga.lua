a3fdb5c2c6e7c7a2d8ffb19f3d363a7ec8f3666b
Igor Zeljkovic <zeljkovic18@gmail.com>
2023-08-23 18:25:04 +0200

    Rename example-settings, save saga colors to variable

--- nvim/lua/config/_saga.lua
+++ nvim/lua/config/_saga.lua
@@ -2,6 +2,20 @@ local saga = require("lspsaga")
 local colors = require("tokyonight.colors").default
 local icons = require("user.icons")
 
+local saga_colors = {
+    normal_bg = colors.bg,
+    red = colors.red,
+    magenta = colors.magenta,
+    orange = colors.orange,
+    yellow = colors.yellow,
+    green = colors.green,
+    cyan = colors.cyan,
+    blue = colors.blue,
+    purple = colors.purple,
+    white = colors.white,
+    black = colors.black,
+}
+
 saga.setup({
     finder = {
         silent = true,
@@ -22,18 +36,5 @@ saga.setup({
         theme = "round",
         code_action = icons.diagnostics.BoldHint,
         border = "rounded",
-        -- colors = {
-        --     normal_bg = colors.bg,
-        --     red = colors.red,
-        --     magenta = colors.magenta,
-        --     orange = colors.orange,
-        --     yellow = colors.yellow,
-        --     green = colors.green,
-        --     cyan = colors.cyan,
-        --     blue = colors.blue,
-        --     purple = colors.purple,
-        --     white = colors.white,
-        --     black = colors.black,
-        -- },
     },
 })
