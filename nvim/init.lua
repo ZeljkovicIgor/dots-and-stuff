65cf1ac70b8fa9faa901c780ab115c0f095e2b1e
Igor Zeljkovic <zeljkovic18@gmail.com>
2023-09-18 03:32:04 +0200

    Refactor plugins code

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
-require("user.plugins")
+require("user.lazy")
 require("user.options")
 require("user.keymaps")
 require("user.lsp")
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,18 +1,4 @@
--- auto install lazy if not installed
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
-local plugins = {
+return {
     "lewis6991/impatient.nvim",
 
     "neovim/nvim-lspconfig", -- Configurations for Nvim LSP
@@ -216,11 +202,3 @@ local plugins = {
         end,
     },
 }
-
-local opts = {
-    ui = {
-        border = "rounded",
-    },
-}
-
-require("lazy").setup(plugins, opts)
