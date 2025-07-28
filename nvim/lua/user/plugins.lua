08aba3f87adfbefd232d7b250632f07e73911865
ZeljkovicIgor <zeljkovic18@gmail.com>
2025-07-28 18:47:15 +0200

    Add nvim dap

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -239,4 +239,26 @@ return {
             })
         end,
     },
+
+    {
+        "mfussenegger/nvim-dap",
+        config = function()
+            local dap = require("dap")
+
+            dap.adapters.php = {
+                type = "executable",
+                command = "node",
+                args = { "/home/fedigor/.local/share/nvim/mason/packages/php-debug-adapter/extension/out/phpDebug.js" },
+            }
+
+            dap.configurations.php = {
+                {
+                    type = "php",
+                    request = "launch",
+                    name = "Listen for Xdebug",
+                    port = 9003,
+                },
+            }
+        end,
+    },
 }
